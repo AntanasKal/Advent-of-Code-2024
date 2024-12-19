4fbb935737e362aea70b4055ebc336a9da446b78
Antanas Kalkauskas <a.kalkauskas@gmail.com>
2024-12-19 16:00:08 +0200

    add day15 solution

--- src/Day15.hs
+++ src/Day15.hs
@@ -44,6 +44,29 @@ parseMap input = (mapArray, initialPosition)
         mapArray = array ((0,0), (h,w)) [((i,j), list!! fromIntegral i !! fromIntegral j) | i <- [0..h], j <- [0..w]]
         initialPosition = head $ [(i,j) | i <- [0..h], j <- [0..w], mapArray ! (i,j) == Character ]
 
+convertList :: [[MapElement]] -> [[MapElement2]]
+convertList = map convertLine
+    where
+        convertLine line = concat (convertElement <$> line)
+        convertElement element =
+            case element of
+                Space -> [Space2, Space2]
+                Wall -> [Wall2, Wall2]
+                Box -> [BoxL, BoxR]
+                Character -> [Character2, Space2]
+
+
+
+parseMap2 :: String -> (Array (Integer, Integer) MapElement2, (Integer, Integer))
+parseMap2 input = (mapArray, initialPosition)
+    where
+        list = convertList $ map (map parseMapElement) (lines input)
+        h = fromIntegral $ length list - 1
+        w = fromIntegral $ (length $ head list) - 1
+        mapArray = array ((0,0), (h,w)) [((i,j), list!! fromIntegral i !! fromIntegral j) | i <- [0..h], j <- [0..w]]
+        initialPosition = head $ [(i,j) | i <- [0..h], j <- [0..w], mapArray ! (i,j) == Character2 ]
+
+
 
 parseDirection :: Char -> Direction
 parseDirection '>' = DRight
@@ -60,7 +83,7 @@ moveLeft2 mapArray (i, j) =
     case mapArray ! (i', j') of
         Space2 -> mapArray // [((i',j'), mapArray ! (i,j)), ((i,j), Space2)]
         Wall2 -> mapArray
-        BoxL ->
+        _ ->
             if movedMap ! (i',j') == Space2 then
                 movedMap // [((i',j'), mapArray ! (i,j)), ((i,j), Space2)]
             else
@@ -71,21 +94,66 @@ moveLeft2 mapArray (i, j) =
         movedMap = (moveLeft2 mapArray (i', j'))
 
 
-moveUp2 :: Array (Integer, Integer) MapElement -> (Integer, Integer) -> Array (Integer, Integer) MapElement
+moveUp2 :: Array (Integer, Integer) MapElement2 -> (Integer, Integer) -> Array (Integer, Integer) MapElement2
 moveUp2 mapArray (i, j) =
     case mapArray ! (i', j') of
-        Space -> mapArray // [((i',j'), mapArray ! (i,j)), ((i,j), Space)]
-        Wall -> mapArray
-        Box ->
-            if movedMap ! (i',j') == Space then
-                movedMap // [((i',j'), mapArray ! (i,j)), ((i,j), Space)]
+        Space2 -> mapArray // [((i',j'), mapArray ! (i,j)), ((i,j), Space2)]
+        Wall2 -> mapArray
+        BoxL ->
+            if movedMap1 ! (i',j') == Space2 && movedMap2L ! (i',j'+1) == Space2  then
+                movedMap2L // [((i',j'), mapArray ! (i,j)), ((i,j), Space2)]
+            else
+                mapArray
+        BoxR ->
+            if movedMap1 ! (i',j') == Space2 && movedMap2R ! (i',j'-1) == Space2  then
+                movedMap2R // [((i',j'), mapArray ! (i,j)), ((i,j), Space2)]
             else
                 mapArray
     where
         i' = i-1
         j' = j
-        movedMap = (moveUp mapArray (i', j'))
+        movedMap1 = moveUp2 mapArray (i', j')
+        movedMap2L = moveUp2 movedMap1 (i', j'+1)
+        movedMap2R = moveUp2 movedMap1 (i', j'-1)
 
+
+moveDown2 :: Array (Integer, Integer) MapElement2 -> (Integer, Integer) -> Array (Integer, Integer) MapElement2
+moveDown2 mapArray (i, j) =
+    case mapArray ! (i', j') of
+        Space2 -> mapArray // [((i',j'), mapArray ! (i,j)), ((i,j), Space2)]
+        Wall2 -> mapArray
+        BoxL ->
+            if movedMap1 ! (i',j') == Space2 && movedMap2L ! (i',j'+1) == Space2  then
+                movedMap2L // [((i',j'), mapArray ! (i,j)), ((i,j), Space2)]
+            else
+                mapArray
+        BoxR ->
+            if movedMap1 ! (i',j') == Space2 && movedMap2R ! (i',j'-1) == Space2  then
+                movedMap2R // [((i',j'), mapArray ! (i,j)), ((i,j), Space2)]
+            else
+                mapArray
+    where
+        i' = i+1
+        j' = j
+        movedMap1 = moveDown2 mapArray (i', j')
+        movedMap2L = moveDown2 movedMap1 (i', j'+1)
+        movedMap2R = moveDown2 movedMap1 (i', j'-1)
+
+
+moveRight2 :: Array (Integer, Integer) MapElement2 -> (Integer, Integer) -> Array (Integer, Integer) MapElement2
+moveRight2 mapArray (i, j) =
+    case mapArray ! (i', j') of
+        Space2 -> mapArray // [((i',j'), mapArray ! (i,j)), ((i,j), Space2)]
+        Wall2 -> mapArray
+        _ ->
+            if movedMap ! (i',j') == Space2 then
+                movedMap // [((i',j'), mapArray ! (i,j)), ((i,j), Space2)]
+            else
+                mapArray
+    where
+        i' = i
+        j' = j+1
+        movedMap = (moveRight2 mapArray (i', j'))
 
 
 moveUp :: Array (Integer, Integer) MapElement -> (Integer, Integer) -> Array (Integer, Integer) MapElement
@@ -158,6 +226,14 @@ move (mapArray, (i,j)) DLeft = (moveLeft mapArray (i,j), if moveLeft mapArray (i
 move (mapArray, (i,j)) DRight = (moveRight mapArray (i,j), if moveRight mapArray (i,j) ! (i,j) == Character then (i,j) else (i,j+1))
 
 
+
+move2 :: (Array (Integer, Integer) MapElement2, (Integer, Integer)) -> Direction -> (Array (Integer, Integer) MapElement2, (Integer, Integer))
+move2 (mapArray, (i,j)) DUp = (moveUp2 mapArray (i,j), if moveUp2 mapArray (i,j) ! (i,j) == Character2 then (i,j) else (i-1,j))
+move2 (mapArray, (i,j)) DDown = (moveDown2 mapArray (i,j), if moveDown2 mapArray (i,j) ! (i,j) == Character2 then (i,j) else (i+1,j))
+move2 (mapArray, (i,j)) DLeft = (moveLeft2 mapArray (i,j), if moveLeft2 mapArray (i,j) ! (i,j) == Character2 then (i,j) else (i,j-1))
+move2 (mapArray, (i,j)) DRight = (moveRight2 mapArray (i,j), if moveRight2 mapArray (i,j) ! (i,j) == Character2 then (i,j) else (i,j+1))
+
+
 findCharacter :: Array (Integer, Integer) MapElement -> (Integer, Integer)
 findCharacter mapArray = head [(i,j) | i <- [0..h], j<-[0..w], mapArray ! (i,j) == Character]
     where
@@ -170,25 +246,47 @@ getFinalNumber mapArray =  sum [100*i + j | i <- [0..h], j<-[0..w], mapArray ! (
     where
         (_, (h, w)) = bounds mapArray
 
+getFinalNumber2 :: Array (Integer, Integer) MapElement2 -> Integer
+getFinalNumber2 mapArray =  sum [100*i + j | i <- [0..h], j<-[0..w], mapArray ! (i,j) == BoxL]
+    where
+        (_, (h, w)) = bounds mapArray
+
 
 solvePart1 :: String -> IO Integer
 solvePart1 path = do
     file <- readFile path
     let (mapArray, initialCharPos) = parseMap $ head (splitOn "\n\n" file)
     let directions = parseDirections $ concat $ lines $ splitOn "\n\n" file !! 1
-    print directions
-    print $ mapArray
-    -- print $ move (mapArray, initialCharPos) DLeft 
     let (finalMap, pos) = foldl move (mapArray, initialCharPos) directions
-    print $ getFinalNumber finalMap
+    return $ getFinalNumber finalMap
 
-    -- print directions
-    return 1
+
+displayMap :: Array (Integer, Integer) MapElement2 -> String
+displayMap mapArray = unlines  [ [elementToChar (mapArray ! (i,j)) | j <- [0..w]] | i <- [0..h]]
+    where
+        (_, (h, w)) = bounds mapArray
+        elementToChar c = case c of
+            BoxL -> '['
+            BoxR -> ']'
+            Space2 -> '.'
+            Character2 -> '@'
+            Wall2 -> '#'
+
 
 solvePart2 :: String -> IO Integer
 solvePart2 path = do
     file <- readFile path
-    return 2
+    let (mapArray, initialCharPos) = parseMap2 $ head (splitOn "\n\n" file)
+    let directions = parseDirections $ concat $ lines $ splitOn "\n\n" file !! 1
+    print directions
+    print $ mapArray
+    -- print $ move (mapArray, initialCharPos) DLeft 
+    let (finalMap, pos) = foldl move2 (mapArray, initialCharPos) directions
+    print finalMap
+    -- putStrLn $ displayMap finalMap
+    -- print $ getFinalNumber2 finalMap
+
+    return $ getFinalNumber2 finalMap
 
 main :: IO ()
 main = do
